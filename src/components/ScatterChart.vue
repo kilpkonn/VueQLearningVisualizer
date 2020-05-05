470a84e2591e29c05ca28c9eb76f19adc93d9185
taannu <tavo.annus@gmail.com>
2020-05-05 13:01:17 +0300

    Add scatter chart

---
+++ src/components/ScatterChart.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="echarts">
+        <IEcharts
+                :option="bar"
+                :loading="loading"
+                @ready="onReady"
+                @click="onClick"
+        />
+        <button @click="doRandom">Random</button>
+    </div>
+</template>
+
+<script type="text/babel">
+  import IEcharts from 'vue-echarts-v3/src/full.js';
+  import * as ecStat from "echarts-stat";
+
+  export default {
+    name: 'bar',
+    components: {
+      IEcharts
+    },
+    props: {},
+    data: () => ({
+      loading: false,
+      bar: {
+        title: {
+          text: 'ECharts Hello World'
+        },
+        tooltip: {},
+        xAxis: {
+          data: [0]
+        },
+        yAxis: {},
+        series: [{
+          name: 'Results',
+          type: 'scatter',
+          data: [0],
+        },
+          {
+            name: 'line',
+            type: 'line',
+            smooth: true,
+            showSymbol: false,
+            data: [0],
+            markPoint: {
+              itemStyle: {
+                color: 'transparent'
+              },
+              label: {
+                show: true,
+                position: 'left',
+                formatter: null,
+                color: '#333',
+                fontSize: 14
+              },
+              data: [{
+                coord: []
+              }]
+            }
+          }]
+      }
+    }),
+    async mounted() {
+      this.loadData();
+    },
+    methods: {
+      doRandom() {
+        const that = this;
+        let data = [];
+        for (let i = 0, min = 5, max = 99; i < 6; i++) {
+          data.push(Math.floor(Math.random() * (max + 1 - min) + min));
+        }
+        that.loading = !that.loading;
+        that.bar.series[0].data = data;
+      },
+      onReady(instance, ECharts) {
+        console.log(instance, ECharts);
+      },
+      onClick(event, instance, ECharts) {
+        console.log(event, instance, ECharts);
+      },
+      async loadData() {
+        fetch("/data/result_list.json")
+          .then((res) => res.json())
+          .then((res) => {
+            const that = this;
+            let xData = [];
+            let regData = [];
+            for (let i = 0; i < res.length; i++) {
+              xData.push(i);
+              regData.push([i, res[i]])
+            }
+            that.bar.xAxis.data = xData;
+            that.bar.series[0].data = res;
+
+            const myRegression = ecStat.regression('polynomial', regData, 3);
+
+            myRegression.points.sort(function (a, b) {
+              return a[0] - b[0];
+            });
+            that.bar.series[1].data = myRegression.points;
+
+            that.bar.series[1].markPoint.data = [{
+              coord: myRegression.points[myRegression.points.length - 1]
+            }];
+            that.bar.series[1].markPoint.label.formatter = myRegression.expression;
+          })
+      }
+    }
+  };
+</script>
+
+<style scoped>
+    .echarts {
+        width: 400px;
+        height: 400px;
+    }
+</style>
